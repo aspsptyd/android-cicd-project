d02746ab92a7ed943292cc8a514ba0d00af0218c
aspsptyd <devadiroot@gmail.com>
2024-05-22 05:41:35 +0700

    Add: layout Row dengan spacer & bagaimana mengimport dp

--- app/src/main/java/id/aspsptyd/deck_project/MainActivity.kt
+++ app/src/main/java/id/aspsptyd/deck_project/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import id.aspsptyd.deck_project.ui.theme.DeckprojectTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,8 +36,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UserProfile() {
-    Column {
+    Row {
         Text(text = "Asep Septiadi")
+        Spacer(modifier = Modifier.size(4.dp))
         Text(text = "UID 44342")
     }
 }
